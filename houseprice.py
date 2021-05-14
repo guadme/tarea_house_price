229ae73f5aebb9ddc7c5855e84587b7a8f484410
Guillermo Adam <guille98.uv@gmail.com>
2021-05-14 17:43:52 +0200

    Imports and load csv

--- houseprice.py
+++ houseprice.py
@@ -8,5 +8,29 @@ MDA
 
 """
 
+import os  #importamos sistema operativo para gestionar directorios
+import pandas as pd #importamos panda con el nick pd
+import numpy as np #importamos numpy para operaciones sobre vectores y matrices
+import matplotlib.pyplot as plt #importamos matplotlib para plots
+import datetime as dt
+import seaborn as sn
+from pandas.api.types import CategoricalDtype #For definition of custom categorical data types (ordinal if necesary)
+import scipy.stats as stats  # For statistical inference 
+
+
+#%% Ingesta de datos
+
+os.chdir(r'C:\Users\guill\Desktop\master\ejercicio_house_price\tarea_house_price')
+
+houseprices = pd.read_csv(r'C:\Users\guill\Desktop\master\ejercicio_house_price\tarea_house_price\resources\houseprice.csv')
+
+
+
+#%% Exploración gráfica del objetivo (precio de la vivienda).
+
+
+
+
+
 
 
